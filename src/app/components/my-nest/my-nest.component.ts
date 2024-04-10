f79109f52c06f86986a74dcf8e6ef5ac5f3ed88e
Edgard Alvarado <edgard-0211@hotmail.com>
2024-04-10 13:15:19 -0500

    progress get unique conversation

--- src/app/components/my-nest/my-nest.component.ts
+++ src/app/components/my-nest/my-nest.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SummaryConversations } from 'src/app/interfaces/summary-conversations';
+import { UniqueConversation } from 'src/app/interfaces/unique-conversation';
 import { ConversationService } from 'src/app/services/conversation.service';
 
 @Component({
@@ -8,20 +9,23 @@ import { ConversationService } from 'src/app/services/conversation.service';
   styleUrls: ['./my-nest.component.css']
 })
 export class MyNestComponent implements OnInit {
-private userId = 'testing-from-angularApp';
-public conversations: SummaryConversations[]
+  private userId = 'testing-from-angularApp';
+  public conversations: SummaryConversations[];
+  public conversation: UniqueConversation;
+
 
   constructor(
     private service: ConversationService
   ) {
     this.conversations = [];
+    this.conversation = this.emptyConversations();
   }
 
   ngOnInit(): void {
     this.loadConversations();
   }
 
-  loadConversations(){
+  loadConversations() {
     this.service.getConversations(this.userId).subscribe(
       (data) => {
         console.log(data);
@@ -29,7 +33,26 @@ public conversations: SummaryConversations[]
       });
   }
 
-  emptySummaryConversations(){
+  loadConversation(conversationId: string) {
+    this.service.getConversation(this.userId, conversationId).subscribe(
+      (data) => {
+        console.log(data);
+        this.conversation = data;
+      }
+    );
+
+  }
+
+  emptySummaryConversations() {
     return "test";
+  }
+
+  emptyConversations(): UniqueConversation {
+    return {
+      id: '',
+      totalMessages: 0,
+      createdAt: new Date(),
+      message: []
+    };
   }
 }
