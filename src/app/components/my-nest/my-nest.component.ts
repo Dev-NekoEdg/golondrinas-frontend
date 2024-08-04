daf4e81a17de6ee8ea45e316ca7056e0fc90ea88
Edgard Alvarado <edgard-0211@hotmail.com>
2024-08-04 11:56:13 -0500

    test

--- src/app/components/my-nest/my-nest.component.ts
+++ src/app/components/my-nest/my-nest.component.ts
@@ -1,24 +1,21 @@
-import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { SummaryConversations } from 'src/app/interfaces/summary-conversations';
 import { Message, UniqueConversation } from 'src/app/interfaces/unique-conversation';
 import { ConversationService } from 'src/app/services/conversation.service';
 import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
-import { ScrollToBottomDirective } from 'src/directives/scroll-to-bottom.directive';
 
 @Component({
   selector: 'app-my-nest',
   templateUrl: './my-nest.component.html',
   styleUrls: ['./my-nest.component.css']
 })
-export class MyNestComponent implements OnInit, AfterViewInit {
+export class MyNestComponent implements OnInit {
   private userId = 'mock-user-angularApp';
   public conversations: SummaryConversations[];
   public conversation: UniqueConversation;
   public conversationActive: boolean;
   public conversationId: string;
   public formNewText: FormGroup
-  //@ts-ignore
-  @ViewChild(ScrollToBottomDirective) scroll: ScrollToBottomDirective;
 
   constructor(
     private elRef: ElementRef,
@@ -30,11 +27,6 @@ export class MyNestComponent implements OnInit, AfterViewInit {
     this.conversationActive = false;
     this.conversationId = '';
     this.formNewText = this.createForm();
-  }
-  ngAfterViewInit(): void {
-    // setTimeout(() => {
-    //   this.setScrollAtTheEnd()
-    // }, 1000);
   }
 
   ngOnInit(): void {
@@ -55,7 +47,7 @@ export class MyNestComponent implements OnInit, AfterViewInit {
     });
   }
 
-  loadConversation(conversationId: string) {
+  loadConversation(conversationId: string): void {
     console.log('load conversation with id: ' + conversationId);
     this.conversationId = conversationId;
     this.conversationActive = true;
@@ -69,9 +61,6 @@ export class MyNestComponent implements OnInit, AfterViewInit {
       }
     );
 
-    setTimeout(() => {
-      this.setScrollAtTheEnd()
-    }, 500);
   }
 
   sendNewMessage() {
@@ -90,14 +79,7 @@ export class MyNestComponent implements OnInit, AfterViewInit {
 
     // save throught
     this.conversation.messages.push(newMessage);
-    // setTimeout(() => {
-    //   this.setScrollAtTheEnd()
-    // }, 1000);
 
-  }
-
-  emptySummaryConversations() {
-    return "test";
   }
 
   emptyConversations(): UniqueConversation {
@@ -109,29 +91,7 @@ export class MyNestComponent implements OnInit, AfterViewInit {
     };
   }
 
-  setScrollAtTheEnd(): void {
-    //divDisplayConversation
-    const divMessages = document.getElementsByClassName('messageBoxDefault');
-    console.log({ divMessages });
-    if (divMessages !== null && divMessages !== undefined) {
-      console.log({ 'divMessages.length': divMessages.length });
-      const lastItem: any = divMessages[(divMessages.length - 1)];
-      console.log({ lastItem });
-      let topPost = lastItem.offsetTop;
-      console.log({ topPost });
-      //@ ts-ignore
-      const x = document.getElementById('divDisplayConversation');
-      console.log({ x });
-      if (x !== null && x !== undefined) {
-        x.scrollTop = topPost;
-        //@ts-ignore
-        document.getElementById('divDisplayConversation').scrollTop = topPost;
-        const t = document.getElementById('divDisplayConversation');
-        console.log({ t });
-      }
-      //document.getElementById('divDisplayConversation')?.scrollTop = topPost;
-    }
-  }
+
 
 
 }
